0be9e7a6e627c52ba746387222966c0242f02abe
ladno07 <raiizmine@gmail.com>
2026-04-26 22:46:34 +0500

    add srgb color space

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -12,7 +12,7 @@ import React, { useEffect } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 import useMacbookStore from '../../store'
 import { noChangeParts } from '../../constants'
-import { Color} from 'three'
+import { Color, SRGBColorSpace} from 'three'
 
 export default function MacbookModel14(props) {
 
@@ -20,7 +20,8 @@ export default function MacbookModel14(props) {
   const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb')
 
   const texture = useTexture('/screen.png')
-
+  texture.colorSpace = SRGBColorSpace
+  texture.needsUpdate = true
   useEffect(()=>{
     scene.traverse((child) =>{
       if(child.isMesh){
