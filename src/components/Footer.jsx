088d829b21ea7c93c9899600c299ec28b0d7162c
ladno07 <raiizmine@gmail.com>
2026-04-29 19:06:19 +0500

    Implement Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
-const Footer = () =>{
-  return (
-    <section>
+import { footerLinks } from "../constants"
 
-    </section>
+const Footer = () => {
+  return (
+    <footer>
+      <div id="info">
+        <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
+        <img src="/logo.svg" alt="Apple logo"/>
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({label,link}) =>(
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 export default Footer
--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -1,15 +1,13 @@
-import { useRef } from "react";
-import { useMediaQuery } from "react-responsive";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import { performanceImages, performanceImgPositions } from "../constants";
-
-gsap.registerPlugin(ScrollTrigger);
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+import { performanceImages, performanceImgPositions } from "../constants"
 
 const Performance = () => {
-  const sectionRef = useRef(null);
-  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+  const sectionRef = useRef(null)
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" })
 
   useGSAP(
     () => {
@@ -29,9 +27,9 @@ const Performance = () => {
             invalidateOnRefresh: true,
           },
         },
-      );
+      )
 
-      if (isMobile) return;
+      if (isMobile) return
 
       const tl = gsap.timeline({
         defaults: { ease: "power1.inOut", duration: 2, overwrite: "auto" },
@@ -42,48 +40,44 @@ const Performance = () => {
           scrub: 1,
           invalidateOnRefresh: true,
         },
-      });
+      })
 
       performanceImgPositions.forEach((pos) => {
-        if (pos.id === "p5") return;
+        if (pos.id === "p5") return
 
-        const toVars = {};
-        if (pos.left !== undefined) toVars.left = `${pos.left}%`;
-        if (pos.right !== undefined) toVars.right = `${pos.right}%`;
-        if (pos.bottom !== undefined) toVars.bottom = `${pos.bottom}%`;
-        if (pos.transform !== undefined) toVars.transform = pos.transform;
+        const toVars = {}
+        if (pos.left !== undefined) toVars.left = `${pos.left}%`
+        if (pos.right !== undefined) toVars.right = `${pos.right}%`
+        if (pos.bottom !== undefined) toVars.bottom = `${pos.bottom}%`
+        if (pos.transform !== undefined) toVars.transform = pos.transform
 
-        tl.to(`.${pos.id}`, toVars, 0);
-      });
-    },{scope:sectionRef,dependencies:[isMobile]}
-  );
+        tl.to(`.${pos.id}`, toVars, 0)
+      })
+    },
+    { scope: sectionRef, dependencies: [isMobile] },
+  )
 
   return (
     <section id="performance" ref={sectionRef}>
       <h2>Next-level graphics performance. Game on.</h2>
 
       <div className="wrapper">
-        {performanceImages.map(({ id, src }) => (
-          <img key={id} src={src} alt={id} className={id} />
-        ))}
+        <div className="wrapper">
+          {performanceImages.map(({ id, src }) => (
+            <img key={id} src={src} alt={id} className={id} />
+          ))}
+        </div>
       </div>
 
       <div className="content">
         <p>
-          Run graphics-intensive workflows with a responsiveness that keeps up
-          with your imagination. The M4 family of chips features a GPU with a
-          second-generation hardware-accelerated ray tracing engine that renders
-          images faster, so{" "}
-          <span className="text-white">
-            gaming feels more immersive and realistic than ever.
-          </span>{" "}
-          And Dynamic Caching optimizes fast on-chip memory to dramatically
-          increase average GPU utilization - driving a huge performance boost
-          for the most demanding pro apps and games.
+          Run graphics-intensive workflows with a responsiveness that keeps up with your imagination. The M4 family of chips features a GPU with a second-generation hardware-accelerated ray tracing
+          engine that renders images faster, so <span className="text-white">gaming feels more immersive and realistic than ever.</span> And Dynamic Caching optimizes fast on-chip memory to
+          dramatically increase average GPU utilization - driving a huge performance boost for the most demanding pro apps and games.
         </p>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Performance;
+export default Performance
